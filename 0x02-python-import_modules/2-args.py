2fbb865c2ce1516573cc0f0739b8ab9b18e3f943
Nellydhlamini <nellydhlamini34@gmail.com>
2023-09-07 06:56:52 -0700

    a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
 
     if l_av > 1:
         print(l_av, "arguments:")
-        for i in range(l_av + 1):
+        for i in range(1, l_av + 1):
             print("{:d}: {}".format(i, av[i]))
     elif l_av == 1:
-        print("{:d}: {}".format(i, av[i]))
-        for i in range(l_av + 1):
+        print(l_av, "argument:")
+        for i in range(1, l_av + 1):
             print("{:d}: {}".format(i, av[i]))
     elif l_av == 0:
         print(l_av, "argument.")
