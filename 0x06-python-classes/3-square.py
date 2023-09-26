a723106a2e98efa36fc155d68e991b4d7fcdcbed
Nellydhlamini <nellydhlamini34@gmail.com>
2023-09-26 13:25:55 -0700

    a class Square that defines a square by: (based on 2-square.py)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+A Square class.
+"""
+class Square:
+    """
+    Private instance attribute: size
+    Instantiation with optional size:
+    def __init__(self, size=0):
+    size must be an integer, otherwise raise a TypeError exception
+    with the message size must be an integer
+    if size is less than 0, raise a ValueError exception with the
+    message size must be >= 0
+    Public instance method: def area(self):
+    that returns the current square area
+    """
+
+    def __init__(self, size=0):
+        """__init__
+
+        The __init__ method initializes the size value of the square.
+
+        Attributes:
+            size (:obj:`int`, optional): The size of the square.
+
+        Raises:
+            TypeError: If `size` type is not `int`.
+
+            ValueError: If `size` is less than `0`.
+
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area
+
+        """
+        return self.__size ** 2
