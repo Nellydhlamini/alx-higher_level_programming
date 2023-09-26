97d63ba5dfd60fc567154548d41d35c57d664eb6
Nellydhlamini <nellydhlamini34@gmail.com>
2023-09-26 13:12:18 -0700

    a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
     Instantiation with optional size: def __init__(self, size=0):
     size must be an integer, otherwise raise a TypeError exception with
     the message size must be an integer
-    if size is less than 0, raise a ValueError exception with the 
+    if size is less than 0, raise a ValueError exception with the
     message size must be >= 0
     """
 
