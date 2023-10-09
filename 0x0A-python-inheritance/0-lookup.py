b21a9fba0399ea75ceefd4f731beb3f092b92ee3
Nellydhlamini <nellydhlamini34@gmail.com>
2023-10-09 19:55:38 +0000

    first commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""It defines an object attribute"""
+
+
 def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object
-    """
+    """Returns the list of available attributes and methods of an object"""
 
     return dir(obj)
